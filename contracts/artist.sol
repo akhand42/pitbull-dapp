ae7ca98780bd3e15cfd138b5b3de7cdcfecda7ef
Albert Chon <achon@stanford.edu>
2018-05-25 01:59:27 -0700

    ERC-721 code written, not tested

--- contracts/artist.sol
+++ contracts/artist.sol
@@ -41,8 +41,6 @@ contract ERC721 {
     // Optional Functions
     function name() public pure returns (string);
     function symbol() public pure returns (string);
-    function tokenOfOwnerByIndex(address _owner, uint256 _index) public view returns (uint256);
-    // function tokenMetadata(uint256 _tokenId) public view returns (string infoUrl);
 
     // Required Events
     event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
@@ -62,7 +60,7 @@ contract ArtistToken is AccessControl, ERC721 {
   ArtistToken[] public artist;
   mapping (uint256 => address) public artistTokenIdToOwner;
   mapping (address => uint256[]) internal ownerToArtistTokens;
-
+  mapping (uint256 => address) public artistTokenToApproved;
   // Constructor
   function ArtistToken(uint256 minTokenPrice) public {
     owner = msg.sender;
@@ -104,10 +102,14 @@ contract ArtistToken is AccessControl, ERC721 {
   }
 
   function transfer(address _to, uint256 _tokenId) public {
+    _transferToken(msg.sender, _to, _tokenId);
+  }
+
+  function _transferToken(address _from, address _to, uint256 _tokenId) internal{
     require(_to != address(0));
-    require(artistTokenIdToOwner[_tokenId] == msg.sender);
+    require(artistTokenIdToOwner[_tokenId] == _from);
     artistTokenIdToOwner[_tokenId] = _to;
-    uint256[] storage userTokens = ownerToArtistTokens[msg.sender];
+    uint256[] storage userTokens = ownerToArtistTokens[_from];
     for (int i = 0; i < userTokens.length; i++){
       if (userTokens[i] == _tokenId){
         delete userTokens[i];
@@ -115,22 +117,41 @@ contract ArtistToken is AccessControl, ERC721 {
         break;
       }
     }
+    artistTokenToApproved[_tokenId] = 0x0;
     Transfer(msg.sender, _to, _tokenId);
   }
 
-  function approve(address _to, uint256 _tokenId) public {
-    require(tulipToOwner[_tokenId] == msg.sender);
-    tulipToApproved[_tokenId] = _to;
+  function _transferSameTokens(address _to, uint256 _artistId, uint256 number) public returns (bool success) {
+    require(_to != address(0));
+    require(ownerToArtistTokens[msg.sender].length >= number); // sanity check but doesnt guarantee
+    uint _artistIdTokens = 0;
+    uint256[] storage userTokens = ownerToArtistTokens[_from];
+    uint256[] memory indices = new uint256[]; // 453, 24, 2, 222
+    for (uint i = 0; i < userTokens.length; i++){
+      if (artist[userTokens[i]].artistGene == _artistId){
+        indices.push(userTokens[i]);
+      }
+    }
+    if (indices.length >= number){
+      for (uint i = 0; i < number; i++){
+        _transferToken(msg.sender, _to, indices[i]);
+      }
+      return true;
+    }
+    return false;
+  }
 
+  function approve(address _to, uint256 _tokenId) public {
+    require(artistTokenIdToOwner[_tokenId] == msg.sender);
+    artistTokenToApproved[_tokenId] = _to;
     Approval(msg.sender, _to, _tokenId);
   }
 
   function transferFrom(address _from, address _to, uint256 _tokenId) public {
     require(_to != address(0));
-    require(tulipToApproved[_tokenId] == msg.sender);
-    require(tulipToOwner[_tokenId] == _from);
-
-    _transferTulip(_from, _to, _tokenId);
+    require(artistTokenToApproved[_tokenId] == msg.sender);
+    require(artistTokenIdToOwner[_tokenId] == _from);
+    _transferToken(_from, _to, _tokenId);
     Transfer(_from, _to, _tokenId);
   }
 
@@ -140,99 +161,5 @@ contract ArtistToken is AccessControl, ERC721 {
 
   function symbol() public pure returns (string) {
     return "ARTE";
-  }
-
-}
-
-contract TulipSales is TulipToken {
-  event Purchase(address indexed owner, uint256 unitPrice, uint32 amount);
-
-  uint128 public increasePeriod = 6000; // around 1 day
-  uint128 public startBlock;
-  uint256[] public genToStartPrice;
-  uint256[23] internal exp15;
-
-  function renameTulip(uint256 _id, bytes32 _name) public {
-    require(tulipToOwner[_id] == msg.sender);
-
-    tulips[_id].name = _name;
-  }
-
-  function withdrawBalance(uint256 _amount) external onlyAdmins {
-    require(_amount <= this.balance);
-
-    msg.sender.transfer(_amount);
-  }
-contract TulipCore is TulipSales {
-  event ContractUpgrade(address newContract);
-  event MaintenanceUpdate(bool maintenance);
-
-  bool public underMaintenance = false;
-  bool public deprecated = false;
-  address public newContractAddress;
-
-  function TulipCore() public {
-    owner = msg.sender;
-  }
-
-  function getTulip(uint256 _id) public view returns (
-    uint256 genes,
-    uint256 createTime,
-    string name
-  ) {
-    Tulip storage tulip = tulips[_id];
-    genes = tulip.genes;
-    createTime = tulip.createTime;
-
-    bytes memory byteArray = new bytes(32);
-    for (uint8 i = 0; i < 32; i++) {
-      byteArray[i] = tulip.name[i];
-    }
-    name = string(byteArray);
-  }
-
-  function myTulips() public view returns (uint256[]) {
-    uint256[] memory tulipsMemory = ownerToTulips[msg.sender];
-    return tulipsMemory;
-  }
-
-  function myTulipsBatched(uint256 _startIndex, uint16 _maxAmount) public view returns (
-    uint256[] tulipIds,
-    uint256 amountRemaining
-  ) {
-    uint256[] storage tulipArr = ownerToTulips[msg.sender];
-    int256 j = int256(tulipArr.length) - 1 - int256(_startIndex);
-    uint256 amount = _maxAmount;
-
-    if (j < 0) {
-      return (
-        new uint256[](0),
-        0
-      );
-    } else if (j + 1 < _maxAmount) {
-      amount = uint256(j + 1);
-    }
-    uint256[] memory resultIds = new uint256[](amount);
-
-    for (uint16 i = 0; i < amount; i++) {
-      resultIds[i] = tulipArr[uint256(j)];
-      j--;
-    }
-
-    return (
-      resultIds,
-      uint256(j+1)
-    );
-  }
-
-  function setMaintenance(bool _underMaintenance) public onlyAdmins {
-    underMaintenance = _underMaintenance;
-    MaintenanceUpdate(underMaintenance);
-  }
-
-  function upgradeContract(address _newContractAddress) public onlyAdmins {
-    newContractAddress = _newContractAddress;
-    deprecated = true;
-    ContractUpgrade(_newContractAddress);
   }
 }
