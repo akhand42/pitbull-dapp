cddf54611d0afae43b54145f24d8ffc191bbe128
Albert Chon <achon@stanford.edu>
2018-05-25 05:27:17 -0700

    added safemath

--- contracts/ArtistTokenContract.sol
+++ contracts/ArtistTokenContract.sol
@@ -1,5 +1,52 @@
 pragma solidity ^0.4.21;
-import './SafeMath.sol';
+
+
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
+ */
+library SafeMath {
+
+  /**
+  * @dev Multiplies two numbers, throws on overflow.
+  */
+  function mul(uint256 a, uint256 b) internal pure returns (uint256 c) {
+    if (a == 0) {
+      return 0;
+    }
+    c = a * b;
+    assert(c / a == b);
+    return c;
+  }
+
+  /**
+  * @dev Integer division of two numbers, truncating the quotient.
+  */
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    // uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return a / b;
+  }
+
+  /**
+  * @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+  */
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  /**
+  * @dev Adds two numbers, throws on overflow.
+  */
+  function add(uint256 a, uint256 b) internal pure returns (uint256 c) {
+    c = a + b;
+    assert(c >= a);
+    return c;
+  }
+}
+
 
 contract AccessControl {
   address public owner;
@@ -49,7 +96,7 @@ contract ERC721 {
 }
 
 contract ArtistTokenContract is AccessControl, ERC721 {
-
+  using SafeMath for uint256;
   address owner;
   uint256 minTokenPrice;
   uint256 tokenCount;
@@ -94,8 +141,7 @@ contract ArtistTokenContract is AccessControl, ERC721 {
   // Artists call this function to create their own ICO.
   function registerArtist(bytes32 _name, uint256 count, uint256 minPrice) public payable returns
    (uint256 artistGene){
-    // TODO: Overflow fix!!!!
-    require(msg.value >= count * minTokenPrice);
+    require(msg.value >= SafeMath.mul(count, minTokenPrice));
     artistGene = artistCount; // e.g. 1 for KanyeToken, 2 for DiddyToken
     artistCount += 1;
     for (uint i = 0; i < count; i++){
@@ -216,8 +262,7 @@ contract ArtistTokenContract is AccessControl, ERC721 {
   function buyArtistTokensFromUser(uint256 artistGene, uint256 quantity, uint256 buyPrice, address seller, uint256 sellPrice) public payable returns (bool success){
     uint256[] storage singleUserArtistTokens = ownerToArtistGeneMap[seller][artistGene];
     require(singleUserArtistTokens.length >= quantity);
-    // TODO: overflow protection safemath
-    uint256 totalEth = quantity * buyPrice;
+    uint256 totalEth = SafeMath.mul(quantity,buyPrice);
     uint256 forSaleCount = 0;
     for (uint256 i = 0; i < singleUserArtistTokens.length; i++){
       if (artist[singleUserArtistTokens[j]].forSale == true){
